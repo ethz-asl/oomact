0c6795e41b070cb8eee2581b5504e760d970c08f
HannesSommer <hannes.sommer@mavt.ethz.ch>
2017-04-09 10:13:35 +0200

    Print Jacobian dimensions while optimizing in BatchCalibrator

--- oomact/src/BatchCalibrator.cpp
+++ oomact/src/BatchCalibrator.cpp
@@ -151,8 +151,11 @@ class BatchCalibrator : public virtual BatchCalibratorI, public AbstractCalibrat
     estimate(estConf, problem, problem, [&](){
       boost::shared_ptr<backend::LinearSystemSolver> linearSystemSolver(new aslam::backend::SparseCholeskyLinearSystemSolver());
       linearSystemSolver->setAcceptConstantErrorTerms(options_.getAcceptConstantErrorTerms());
+
       aslam::backend::Optimizer2 opt(sm::BoostPropertyTree(), linearSystemSolver, boost::make_shared<backend::LevenbergMarquardtTrustRegionPolicy>(100));
-      updateOptimizerInspector(problem, false, [](std::ostream &){}, opt.callback());
+      updateOptimizerInspector(problem, false, [&](std::ostream &out){
+          out << "The Jacobian matrix is: " << linearSystemSolver->JRows()<< " x " << linearSystemSolver->JCols();
+      }, opt.callback());
       opt.setProblem(problem.getProblemSp());
       opt.options().verbose = false;
 //      opt.options().maxIterations = _options.maxIterations;
