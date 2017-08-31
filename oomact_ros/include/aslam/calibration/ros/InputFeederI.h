085c9dc09fcd58fb0da5d56c9fc58ce83dbbb1d6
HannesSommer <hannes.sommer@mavt.ethz.ch>
2017-08-31 11:46:54 +0200

    Moved InputFeederImpl to InputFeederTemplates

--- oomact_ros/include/aslam/calibration/ros/InputFeederI.h
+++ oomact_ros/include/aslam/calibration/ros/InputFeederI.h
@@ -21,27 +21,6 @@ class InputFeederI {
   virtual void feed(const ::rosbag::MessageInstance & m, ObservationManagerI & obsManager) const = 0;
 };
 
-template <typename Msg, typename Receiver, typename Derived>
-class InputFeederImpl : public InputFeederI {
- public:
-  InputFeederImpl(const Receiver & receiver) : receiver_(receiver) {}
-  virtual ~InputFeederImpl() = default;
-
-  virtual void feed(const ::rosbag::MessageInstance & m, ObservationManagerI & obsManager) const override {
-    auto mInst = m.instantiate<Msg>();
-    if(mInst){ //TODO O : gain efficiency by check out the type of a topic in a bag first and then don't even instantiate the other feeders
-      const Timestamp t = static_cast<const Derived*>(this)->feed(*mInst, receiver_, obsManager);
-      if(t != InvalidTimestamp()){
-        const Sensor & sensor = getSensorFromReceiver(receiver_);
-        VLOG(3) << "Feeding measurement to " << getNameFromSensor(sensor) << " at t=" << obsManager.secsSinceStart(t) << " secs.";
-        obsManager.addMeasurementTimestamp(t, sensor);
-      }
-    }
-  }
- private:
-  const Receiver & receiver_;
-};
-
 } /* namespace ros */
 } /* namespace calibration */
 } /* namespace aslam */
--- oomact_ros/include/aslam/calibration/ros/InputFeederTemplates.h
+++ oomact_ros/include/aslam/calibration/ros/InputFeederTemplates.h
@@ -1,11 +1,54 @@
 #ifndef HB94494AD_043E_41B9_B842_1BDA520FD393
 #define HB94494AD_043E_41B9_B842_1BDA520FD393
 
+#include <stdexcept>
 #include "InputFeederFactoryI.h"
 
 namespace aslam {
 namespace calibration {
 namespace ros {
+
+template <typename Msg, typename Receiver, typename Derived>
+class InputFeederImpl : public InputFeederI {
+ public:
+  InputFeederImpl(const Receiver & receiver) : receiver_(receiver) {}
+  virtual ~InputFeederImpl() = default;
+
+  virtual void feed(const ::rosbag::MessageInstance & m, ObservationManagerI & obsManager) const override final {
+    auto mInst = m.instantiate<Msg>();
+    if(mInst){ //TODO O : gain efficiency by check out the type of a topic in a bag first and then don't even instantiate the other feeders
+      const Timestamp t = getDerived().feedMeasurementAndReturnTimestamp(*mInst, getReceiver(), obsManager);
+      if(t != InvalidTimestamp()){
+        const Sensor & sensor = getSensorFromReceiver(getReceiver());
+        VLOG(3) << "Feeding measurement to " << getNameFromSensor(sensor) << " at t=" << obsManager.secsSinceStart(t) << " secs.";
+        obsManager.addMeasurementTimestamp(t, sensor);
+      }
+    }
+  }
+
+  /**
+   * Feed the measurement from the message to the receiver.
+   * @param m Measurement message
+   * @param receiver
+   * @param obsManager
+   * @return timestamp used for measurement (In receivers time frame). If InvalidTimestamp() is returned the message is assumed skipped.
+   */
+  Timestamp feedMeasurementAndReturnTimestamp(const Msg & m, const Receiver & receiver, ObservationManagerI & obsManager) const {
+    throw std::runtime_error(std::string("Class ") + typeid(Derived).name() + " must implement (shadow) feedMeasurementAndReturnTimestamp.");
+    return InvalidTimestamp();
+  }
+
+
+  const Receiver& getReceiver() const {
+    return receiver_;
+  }
+ protected:
+  const Derived& getDerived() const {
+    return static_cast<const Derived&>(*this);
+  }
+ private:
+  const Receiver & receiver_;
+};
 
 template <typename Msg, typename Measurement>
 class InputFeederForMeassagesWithHeader : public InputFeederImpl<Msg, InputReceiverIT<Measurement>, InputFeederForMeassagesWithHeader<Msg, Measurement>> {
@@ -13,7 +56,8 @@ class InputFeederForMeassagesWithHeader : public InputFeederImpl<Msg, InputRecei
   using InputFeederImpl<Msg, InputReceiverIT<Measurement>, InputFeederForMeassagesWithHeader<Msg, Measurement>>::InputFeederImpl;
   virtual ~InputFeederForMeassagesWithHeader() = default;
 
-  Timestamp feed(const Msg & m, const InputReceiverIT<Measurement> & receiver, ObservationManagerI & obsManager) const {
+  template <typename Receiver>
+  Timestamp feedMeasurementAndReturnTimestamp(const Msg & m, const Receiver & receiver, ObservationManagerI & obsManager) const {
     Measurement measurement;
     bool msg2Measurement(const Msg & m, Measurement&);
     if(!msg2Measurement(m, measurement)){
