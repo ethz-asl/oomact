d70a0ce2e018ecfd821bc6fc8f631586d8ed5141
HannesSommer <hannes.sommer@mavt.ethz.ch>
2017-09-03 12:44:51 +0200

    Also adapt quaternion convention when writing bag files

--- oomact_ros/test/bag_tools.cpp
+++ oomact_ros/test/bag_tools.cpp
@@ -1,7 +1,9 @@
+#include <aslam/calibration/data/PoseMeasurement.h>
 #include <aslam/calibration/test/MockMotionCaptureSource.h>
 #include <aslam/calibration/Timestamp.hpp>
 #include <rosbag/bag.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <sm/kinematics/quaternion_algebra.hpp>
 
 using namespace aslam::calibration;
 
@@ -14,10 +16,18 @@ void writeMotionCaptureSourceToBag(std::string path, std::string topic, const te
     ros::Duration dur;
     dur.fromNSec(m.time.getNumerator());
     ps.header.stamp = ros::TIME_MIN + dur;
-    ps.pose.orientation.x = m.q[0];
-    ps.pose.orientation.y = m.q[1];
-    ps.pose.orientation.z = m.q[2];
-    ps.pose.orientation.w = m.q[3];
+
+    auto q = m.q;
+    static_assert(PoseMeasurement::USE_XYZW_ORDER, "");
+    if(PoseMeasurement::USE_JPL_MULT){
+      q = sm::kinematics::quatInv(q);
+    }
+    static_assert(PoseMeasurement::USE_XYZW_ORDER, "");
+    ps.pose.orientation.x = q[0];
+    ps.pose.orientation.y = q[1];
+    ps.pose.orientation.z = q[2];
+    ps.pose.orientation.w = q[3];
+
     ps.pose.position.x = m.p[0];
     ps.pose.position.x = m.p[1];
     ps.pose.position.x = m.p[2];
